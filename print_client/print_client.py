f333dff38cfeb8d967f28e8b978a1c80d6ed9fb8
BUDS Owner <1522393@gmail.com>
2026-04-26 17:52:14 +0200

    fix(print_client): rotate label 90 degrees for correct orientation

--- print_client/print_client.py
+++ print_client/print_client.py
@@ -79,7 +79,7 @@ def print_label(pdf_bytes: bytes, job_id: str) -> bool:
         import win32print
         import win32ui
         from PIL import ImageWin
-        img = render_pdf_to_image(pdf_bytes).convert("RGB")
+        img = render_pdf_to_image(pdf_bytes).rotate(90, expand=True).convert("RGB")
         hdc = win32ui.CreateDC()
         hdc.CreatePrinterDC(PRINTER_NAME)
         printer_w = hdc.GetDeviceCaps(8)   # HORZRES
